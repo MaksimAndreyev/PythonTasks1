3f7111aa79fd1153e2d5d97d1659ffe504f50f94
maximandreev <maxands@yandex.ru>
2022-10-09 11:22:33 +0300

    Task 4 completed

---
+++ 4.py
@@ -0,0 +1,9 @@
+quarter = int(input('Введите номер четверти: '))
+if quarter == 1:
+    print('x > 0, y > 0')
+elif quarter == 2:
+    print('x < 0, y > 0')
+elif quarter == 3:
+    print('x < 0, y < 0')
+else:
+    print('x > 0, y < 0')
